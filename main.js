550b7139c0194093761bb33f022a44cf80a34180
Leifhaa <leifhaa@hotmail.com>
2021-02-25 22:38:27 +0100

    Reverted structure.

--- main.js
+++ main.js
@@ -10,20 +10,22 @@ const priceCalc = (milliElapsed, initPrice) => {
 }
 
 
-exports.handler = async (event) => {
+exports.handler = function (event, context, callback) {
   const elapsed = Number(event.elapsed)
   const initPrice = Number(event.price)
   const finalPrice = priceCalc(elapsed, initPrice)
 
-  //I'm not persisting the price anywhere, this is just for demonstration.
-  //There could e.g be a database for invoices which we could persist the price.
+  //I'm not persisting the price anywhere, so the calculation is just for demonstration.
+  //There could e.g be a database for invoices which we could persist the final price.
 
-  const response = {
+  var response = {
     statusCode: 200,
-    body: ""
-    ,
-  };
-  return response;
-};
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+    body: '',
+  }
+  callback(null, response)
+}
 
 module.exports = { priceCalc};
